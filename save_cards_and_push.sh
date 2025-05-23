52d7bcb143cba02ded95df9a9deed7a8e74bf381
AlexRz912 <alexandre.rodriguez.arz@gmail.com>
2025-05-23 15:22:42 +0200

    fix save_cards_and_push for autopush of cards backup

--- save_cards_and_push.sh
+++ save_cards_and_push.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-echo "Dossier courant avant le subshell: $(pwd)"
-
 (
     cd /home/$USER/thinkspaced/cards || exit 1
-    echo "Dans le subshell, dossier courant: $(pwd)"
-    # Tes commandes ici :
-    ls
-    cp -r /home/$USER/thinkspaced/cards/routines.json /home/$USER/cards_backup
-    cd /home/$USER/cards_backup
+    cp -r /home/$USER/thinkspaced/cards/routines.json $BACKUP
+    cd $BACKUP
     git add .
     git commit -m "card backup"
-    echo "Fichier temporaire créé dans le subshell"
+    git push
 )
 
-echo "Dossier courant après le subshell: $(pwd)"
